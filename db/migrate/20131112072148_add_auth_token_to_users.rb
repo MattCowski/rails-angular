c00a813c62f862cc13081b8099d8eaeb1169f276
MattCowski <MattCowski@gmail.com>
2013-11-12 15:26:06 -0600

    and deleted files

--- db/migrate/20131112072148_add_auth_token_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthTokenToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :authentication_token, :string
-    #add index because we'll be searching db for this
-    add_index :users, :authentication_token, :unique => true
-  end
-end
