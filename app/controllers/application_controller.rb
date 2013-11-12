31def2c2051e6b6be1e74404aa16832748d33cdf
MattCowski <MattCowski@gmail.com>
2013-11-12 01:21:14 -0600

    app controller checks for CORS and user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,42 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+# skip CSRF
   protect_from_forgery with: :null_session
 
+  #add access controll headers and auth our user
   before_filter :set_cors_headers
   before_filter :cors_preflight
-  # after_filter :set_csrf_cookie_for_ng
 
-  private
-
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  def ping
+    render status: 200,
+        json: {
+          success: "true"
+        }
   end
 
-  def verified_request?
-    super || form_authenticity_token == 
-      request.headers['HTTP_X_XSRF_TOKEN']
+  def set_cors_headers
+  # will set proper HTTP headers for the response from our server
+  # for production, add domain in app_config to enable CORS
+    headers['Access-Control-Allow-Origin'] = AppConfig.client['origin']
+    # OPTION action is for client to see if server has CORS enabled (renders empty page)
+    headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "3628800"
   end
+
+  def cors_preflight
+    #check if user is logged in or not
+    head(:ok) if request.method == :options
+  end
+
+  def authenticate_cors_user
+    if request.xhr? and !user_signed_in?
+      render status: 401,
+            json: {
+              error: "You are not authenticated to run this action"
+            }
+    end
+  end
+
 end
