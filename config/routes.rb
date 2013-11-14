e91e1dea85e839373b705f619f05f7acabaffda0
MattCowski <MattCowski@gmail.com>
2013-11-13 22:29:49 -0600

    finished implementing backend signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,35 @@
 Shareup::Application.routes.draw do
-  namespace :api do
-    resources :shares
-  end
+  scope '/api' do
+    # resources :shares
 
-  devise_for :users,
-    :controllers => {
-      :omniauth_callbacks => "users/omniauth_callbacks"
-    }
+    devise_for :users,
+      :controllers => {
+        omniauth_callbacks: "users/omniauth_callbacks",
+        registrations: "users/registrations",
+        sessions: "users/sessions"
+      }
 
-  devise_scope :user do
-    get '/api/current_user' => 'users/sessions#show_current_user', as: 'show_current_user'
-    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+    # devise_scope :user do
+    #   post '/check/is_user' => 'users/users#is_user', as: 'is_user'
+    #   post '/current_user' => 'users/sessions#get_current_user'
+    # end
   end
+  get '/dashboard' => 'welcome#dashboard'
+  root to: 'welcome#index'
+
+  # namespace :api do
+  #   resources :shares
+  # end
+
+  # devise_for :users,
+  #   :controllers => {
+  #     :omniauth_callbacks => "users/omniauth_callbacks"
+  #   }
+
+  # devise_scope :user do
+  #   get '/api/current_user' => 'users/sessions#show_current_user', as: 'show_current_user'
+  #   post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+  # end
 
   # devise_scope :user do
   #   get '/api/current_user' => 'users/sessions#show_current_user'
@@ -21,8 +39,6 @@ Shareup::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root to: 'welcome#index'
-  get '/dashboard' => 'welcome#dashboard'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
