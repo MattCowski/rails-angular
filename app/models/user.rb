415b7c28cf42ef4fea04f7ea11cf22c48863fb15
MattCowski <MattCowski@gmail.com>
2013-11-13 22:21:07 -0600

    finished authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
          :validatable, :omniauthable
   has_many :authorizations, :dependent => :destroy
   has_many :shares, foreign_key: 'from_user-id'
+  # make sure :authentication_token generates pg 57
+  before_save :ensure_authentication_token
 end
